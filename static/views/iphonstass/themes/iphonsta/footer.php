1bed8d1fed001129b795a9cdfbe5e8b292d6d91c
soddy <soddyque@gmail.com>
2011-03-26 02:31:16 +0800

    完成Feed.修复评论Bug.整理了模板,修复了时间字段名称.

--- static/views/iphonstass/themes/iphonsta/footer.php
+++
@@ -1,9 +0,0 @@
-        </div>
-        <div id="ftr">
-        	<span>Designed by WPSHOWER</span>
-            <a href="#lg" id="top"></a>
-        </div>
-    </div>
-<?php wp_footer(); ?>
-</body>
-</html>
